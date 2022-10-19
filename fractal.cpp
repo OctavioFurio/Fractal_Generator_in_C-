8966e784fc98002b9915284079f7114a8d9aaf09
Octávio Xavier Fúrio <103672525+OctavioFurio@users.noreply.github.com>
2022-10-18 22:02:29 -0300

    Update fractal.cpp
    Optimization of memory consumption and overall efficiency.

--- fractal.cpp
+++ fractal.cpp
@@ -7,11 +7,14 @@ float square(float x) { return x * x; }
 int main() {
 
   int size = 5000;
+  int maxSteps = size / 30;
   int center = size / 2;
-  int color = 0, step = 0;
+  int color = 0, step;
 
-  float x = 0, y = 0;       // Relative positions
-  float x1 = 0, y1 = 0;     // X' and Y' for Julia
+  float x = 0, y = 0;              // Relative positions
+  float x1 = -0.01, y1 = 0.69;     // X' and Y' for Julia
+  float tempX = x;
+  float tempY = y;
 
   BmpImg myImage(size, size);
 
@@ -21,27 +24,24 @@ int main() {
       x = (center - X) / (size / 4.0);
       y = (center - Y) / (size / 4.0);
 
-      x1 = x; // Convert from Julia to Mandelbrot
-      y1 = y; // Comment these lines to render Julia
+      // x1 = x; // Convert from Julia to Mandelbrot
+      // y1 = y; // Un-comment these lines to render Mandelbrot
 
-      if(square(x) + square(y) > 4)
-        myImage.set_pixel((int)X, (int)Y, color, color, color); // Background color
+      step = 1;
+      while (square(x) + square(y) <= 4 && step < maxSteps) {  // 500 steps
+        
+        color = 255 * (float)(((step) % maxSteps) / (float)maxSteps);
 
-      step = 0;
-      while (square(x) + square(y) <= 4 && step < 500) {  // 500 steps
-
-        step++;
-        color = 255 * (float)(((step) % 128) / 128.0);
-
-        float tempX = x;
-        float tempY = y;
-
-        myImage.set_pixel((int)X, (int)Y, color, color, color);
+        tempX = x;
+        tempY = y;
 
         x = square(tempX) - square(tempY) + x1;     // xn = x² - x² + x0
         y = (2 * tempX * tempY) + y1;               // xn = 2xy + y0
 
+        step++;
       }
+            myImage.set_pixel((int)X, (int)Y, color, color/4, color/4);
+    
     }
 
   myImage.write("Output.png");
